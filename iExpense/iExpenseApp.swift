98b11235840e7ed2c41f0bb27eec78383b260c57
DanutPopa <popa.ion.danut@gmail.com>
2024-08-07 08:46:56 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Danut Popa on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
